a8bb8cf76ca199f393b871abe69a9ef2ce7e38b4
kevin paul shaju <kvnpul155@gmail.com>
2021-08-18 15:32:01 +0530

    added login an signup routes. added validations for inputs.

--- src/Controllers/administration/admin.js
+++ src/Controllers/administration/admin.js
@@ -1,19 +1,17 @@
 const Admin = require("../../Models/administration/Admin");
-const isEmpty = require("lodash.isempty");
 const userValidation = require("../../Services/validation");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const jwt = require("jsonwebtoken");
 
 exports.registerAdmin = async (req, res) => {
   const { name, email, gender, phone, role, password, password2 } = req.body;
   // validating the input
   if (!req.body) {
-    return res.status(400).json({ message: "Inputs can not be left empty." });
+    return res.status(406).json({ message: "Inputs can not be left empty." });
   }
   const { error } = userValidation(req.body);
   if (error) {
-    return res.status(400).json({ message: error.details[0].message });
+    return res.status(406).json({ message: error.details[0].message });
   }
 
   try {
@@ -21,24 +19,26 @@ exports.registerAdmin = async (req, res) => {
     const existingAdmin = await Admin.findOne({ email: email });
     if (existingAdmin) {
       return res
-        .status(400)
+        .status(406)
         .json({ message: "This user already exists. Try logging in." });
     }
     // confirming passwords
     else if (password !== password2) {
-      return res.status(400).json({ message: "Passwords do not match." });
+      return res.status(406).json({ message: "Passwords do not match." });
     }
     const newAdmin = new Admin(req.body);
     const savedAdmin = await newAdmin.save();
-    res.json(savedAdmin);
+    res.status(200).json({
+      message: "User has been successfully registered. Please log in",
+    });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };
 
 exports.adminLogin = async (req, res) => {
   if (!req.body) {
-    return res.status(400).json({ message: "Inputs can not be left empty." });
+    return res.status(406).json({ message: "Inputs can not be left empty." });
   }
   const { email, password } = req.body;
 
@@ -47,14 +47,14 @@ exports.adminLogin = async (req, res) => {
     const existingAdmin = await Admin.findOne({ email: email });
     if (!existingAdmin) {
       return res
-        .status(400)
-        .json({ message: "User does not exists.Try signing up." });
+        .status(404)
+        .json({ message: "User does not exist.Try signing up." });
     }
 
     //   match password
     const match = await bcrypt.compare(password, existingAdmin.password);
     if (!match) {
-      return res.status(400).json({
+      return res.status(401).json({
         message: "Invalid password.Please check your password and try again",
       });
     }
@@ -63,8 +63,8 @@ exports.adminLogin = async (req, res) => {
     const key = process.env.JWT_SECRET;
     userId = existingAdmin._id;
     const accessToken = jwt.sign(userId.toString(), key);
-    res.json({ accessToken: accessToken });
+    res.status(200).json({ accessToken: accessToken });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
--- src/Services/validation.js
+++ src/Services/validation.js
@@ -4,7 +4,7 @@ const userValidation = (data) => {
   const schema = Joi.object({
     name: Joi.string().required().min(3).max(20),
     email: Joi.string().required().email(),
-    phone: Joi.string().required().min(10),
+    phone: Joi.string().required().min(10).custom(method, "custom validation"),
     gender: Joi.string().required(),
     role: Joi.string().required(),
     password: Joi.string().required().min(6),
@@ -12,6 +12,24 @@ const userValidation = (data) => {
   });
 
   return schema.validate(data);
+};
+
+
+const regExp = /[a-zA-Z]/g;
+
+const method = (value, helpers) => {
+  // for example if the username value is (something) then it will throw an error with flowing message but it throws an error inside (value) object without error message. It should throw error inside the (error) object with a proper error message
+  
+  if (regExp.test(value)) {
+    // return new Error("Phone number should not contain");
+    return helpers.message({
+      message: "custom validation",
+      custom: '"phone" number should contain only numbers',
+    });
+  }
+  
+  // Return the value unchanged
+  return value;
 };
 
 module.exports = userValidation;
